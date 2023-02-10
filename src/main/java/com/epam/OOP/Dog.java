5db9f33cde73159ee48eaf9d5c8451760d5bb2a4
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-11 03:30:31 +0500

    2-3 done

--- src/main/java/com/epam/OOP/Dog.java
+++ src/main/java/com/epam/OOP/Dog.java
@@ -4,4 +4,9 @@ public class Dog extends Animal {
     public Dog() {
         super("brown", 4, true);
     }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription();
+    }
 }
