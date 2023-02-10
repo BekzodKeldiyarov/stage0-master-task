3fbe07f7880e7db936e3070e65bc149ec8411947
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-11 03:38:18 +0500

    Number reverter done

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,10 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        String numberString = number + "";
+        for (int i = numberString.length() - 1; i >= 0; i--) {
+            System.out.print(numberString.charAt(i));
+        }
     }
 
 }
