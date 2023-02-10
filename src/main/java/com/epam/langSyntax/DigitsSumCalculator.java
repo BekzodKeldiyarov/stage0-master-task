3a1d02e249ae489e235c10067c8e71ccb111c5ce
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-11 03:42:46 +0500

    DigitsSumCalculator done

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,12 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        String s = number + "";
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            sum = sum + Character.getNumericValue(s.charAt(i));
+        }
+        System.out.println(sum);
     }
 
 }
