fec14c98ea767064abbffbc53cc7b76104845807
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-11 03:13:05 +0500

    1-3 done

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -4,4 +4,10 @@ public class Animal {
     private String color;
     private int numberOfPaws;
     private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
 }
