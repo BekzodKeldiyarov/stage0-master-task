d6d1fdabb24708c6265894b6699a1dfb137d22f5
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-12 04:00:44 +0500

    Task-1 Multiplication done

--- src/main/java/com/epam/loops/MultiplicationTable.java
+++ src/main/java/com/epam/loops/MultiplicationTable.java
@@ -1,8 +1,19 @@
 package com.epam.loops;
 
+import java.util.Scanner;
+
 public class MultiplicationTable {
 
     public void printTable(int numberTableToPrint) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(numberTableToPrint + "*" + (i + 1) + "=" + (numberTableToPrint * (i + 1)));
+        }
     }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        MultiplicationTable multiplicationTable = new MultiplicationTable();
+        multiplicationTable.printTable(num);
+    }
 }
