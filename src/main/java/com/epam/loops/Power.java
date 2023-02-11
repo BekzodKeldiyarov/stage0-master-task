6f779185e5d95ea23830ef1531880ac90ad78abe
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-12 03:56:01 +0500

    Task-3 Power done

--- src/main/java/com/epam/loops/Power.java
+++ src/main/java/com/epam/loops/Power.java
@@ -3,11 +3,22 @@ package com.epam.loops;
 public class Power {
 
     public void printPower(int numberToPrint, int power) {
-
+        int result = numberToPrint;
+        for (int i = 0; i < Math.abs(power - 1); i++) {
+            if (power < 0) {
+                result = result / numberToPrint;
+            } else {
+                result = result * numberToPrint;
+            }
+        }
+        if (power == 0) {
+            result = 1;
+        }
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
-        new Power().printPower(10, 3);
+        new Power().printPower(2, 10);
     }
 
 }
